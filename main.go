4ba9f9d537bdd2f71334803a9c393e67b6faf5e6
dionomusuko <ruby.school114@gmail.com>
2020-05-31 10:30:24 +0900

    fix

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func GrassChecker(commit string) string {
+	t := time.Now().Format("2006-01-02")
+	ans := strings.Index(commit, t)
+	if ans == 0 {
+		return "今日も1日お疲れ様でした"
+	} else {
+		return "まだコミットされてません\n" +
+			"プライベートリポジトリにコミット済みの場合はこちらのメッセージを無視してください"
+	}
+}
+
+func main() {
+	fmt.Println(GrassChecker("2020-05-31"))
+}
