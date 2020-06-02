782082c5706d88cd0e53251b19d29382c6f9a010
dionomusuko <ruby.school114@gmail.com>
2020-06-02 10:23:49 +0900

    fix

--- main.go
+++ main.go
@@ -2,21 +2,9 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"time"
+	"github.com/dionomusuko/GrassChecker/encoder"
 )
 
-func GrassChecker(commit string) string {
-	t := time.Now().Format("2006-01-02")
-	ans := strings.Index(commit, t)
-	if ans == 0 {
-		return "今日も1日お疲れ様でした"
-	} else {
-		return "まだコミットされてません\n" +
-			"プライベートリポジトリにコミット済みの場合はこちらのメッセージを無視してください"
-	}
-}
-
 func main() {
-	fmt.Println(GrassChecker("2020-05-31"))
+	fmt.Println(encoder.JsonEncoder("https://api.github.com/users/dionomusuko/repos"))
 }
