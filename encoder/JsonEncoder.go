87428806b5eaa4a3180240306ab88e383eb196ee
dionomusuko <ruby.school114@gmail.com>
2020-06-02 10:42:28 +0900

    fix

--- encoder/JsonEncoder.go
+++ encoder/JsonEncoder.go
@@ -14,31 +14,34 @@ type Commit struct {
 }
 
 func JsonEncoder(url string) string {
-
+	//github.apiからリポジトリ全体のjsonをとってくる
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	client := new(http.Client)
-
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//jsonからpushed_atのみ抜き出す
 	var commits []Commit
+	var tmp string
+
 	if err := json.Unmarshal(body, &commits); err != nil {
 		log.Fatal(err)
 	}
 	t := time.Now().Format("2006-01-02")
-	//var pushed_at []string
-	var tmp string
+
+	//for文を使って今日の日付と一致しているか確認
 	for _, commit := range commits {
 		tmp = commit.PushedAt
 		if strings.Index(tmp, t) == 0 {
