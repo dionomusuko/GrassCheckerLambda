d36db489183fe3bdae0b49582ee57178f1d97773
dionomusuko <ruby.school114@gmail.com>
2020-06-02 10:23:40 +0900

    fix

--- encoder/JsonEncoder.go
+++ encoder/JsonEncoder.go
@@ -5,13 +5,15 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 )
 
 type Commit struct {
 	PushedAt string `json:"pushed_at"`
 }
 
-func JsonEncoder(url string) []Commit {
+func JsonEncoder(url string) string {
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -34,5 +36,16 @@ func JsonEncoder(url string) []Commit {
 	if err := json.Unmarshal(body, &commits); err != nil {
 		log.Fatal(err)
 	}
-	return commits
+	t := time.Now().Format("2006-01-02")
+	//var pushed_at []string
+	var tmp string
+	for _, commit := range commits {
+		tmp = commit.PushedAt
+		if strings.Index(tmp, t) == 0 {
+			break
+			return "お疲れ様でした"
+		}
+	}
+	return "まだコミットされてません\n" +
+		"プライベートリポジトリにコミット済みの場合はこちらのメッセージを無視してください"
 }
