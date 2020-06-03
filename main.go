f9803ca8335eb55e0a74b50ddf6f3c067db62943
dionomusuko <ruby.school114@gmail.com>
2020-06-03 10:09:32 +0900

    add slack

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/dionomusuko/GrassChecker/encoder"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
 )
 
+type payload struct {
+	Text string `json:"text"`
+}
+
 func main() {
-	url := "https://api.github.com/users/dionomusuko/repos"
-	fmt.Println(encoder.JsonEncoder(url))
+	//環境変数の処理
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	github_api := os.Getenv("GITHUB_API")
+	sl_api := os.Getenv("SLACK_API")
+
+	str := encoder.JsonEncoder(github_api)
+	t, err := json.Marshal(payload{Text: str})
+	if err != nil {
+		log.Println(err)
+	}
+	resp, err := http.PostForm(sl_api, url.Values{"payload": {string(t)}})
+	defer resp.Body.Close()
+	log.Println("送信完了")
 }
