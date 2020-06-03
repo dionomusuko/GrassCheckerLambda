417e8f0ab40b09b0a1f7a0db147f35d89fb8548e
dionomusuko <ruby.school114@gmail.com>
2020-06-03 23:22:37 +0900

    add lambda

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/dionomusuko/GrassChecker
 
-require github.com/joho/godotenv v1.3.0 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.17.0
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/stretchr/testify v1.6.0 // indirect
+	github.com/urfave/cli/v2 v2.2.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200602174320-3e3e88ca92fa // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/dionomusuko/GrassChecker/encoder"
 	"github.com/dionomusuko/GrassChecker/slack"
-	"github.com/joho/godotenv"
-	"log"
+	//"github.com/joho/godotenv"
 	"os"
 )
 
-func main() {
+func HandleLambdaEvent() error {
 	//環境変数の処理
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	//err := godotenv.Load()
+	//if err != nil {
+	//	log.Fatal("Error loading .env file")
+	//}
 	github_api := os.Getenv("GITHUB_API")
 	str := encoder.JsonEncoder(github_api)
 	slack.Send(str)
+	return nil
+}
 
+func main() {
+	lambda.Start(HandleLambdaEvent())
 }
