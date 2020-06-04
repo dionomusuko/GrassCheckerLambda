3e77cdaf4f350d54537b7d36d830536708c51e7c
dionomusuko <ruby.school114@gmail.com>
2020-06-04 21:39:46 +0900

    fix

--- main.go
+++ main.go
@@ -4,16 +4,10 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/dionomusuko/GrassChecker/encoder"
 	"github.com/dionomusuko/GrassChecker/slack"
-	//"github.com/joho/godotenv"
 	"os"
 )
 
 func HandleLambdaEvent() error {
-	//環境変数の処理
-	//err := godotenv.Load()
-	//if err != nil {
-	//	log.Fatal("Error loading .env file")
-	//}
 	github_api := os.Getenv("GITHUB_API")
 	str := encoder.JsonEncoder(github_api)
 	slack.Send(str)
